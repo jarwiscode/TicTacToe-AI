56cea26a53a4726d61fb7487ada1f80aff260980
jarwiscode <dz.ibragimov@yandex.ru>
2023-08-21 02:52:57 +0300

    optimization code

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-let boardData = [
-[0,0,0],
-[0,0,0],
-[0,0,0]
-]
-
-let player = 1;
-let gameOver = false;
-
 const cellElements = document.querySelectorAll(".cell");
 const resultElement = document.getElementById("result");
+const restartButton = document.getElementById("restart");
 
+const EMPTY = 0;
+const PLAYER_X = 1;
+const PLAYER_O = -1;
+const GAME_STATUS = {
+    DRAW: 0,
+    X_WINS: 1,
+    O_WINS: 2
+};
+
+let boardData = [
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY]
+];
+
+let player = PLAYER_X;
+let gameOver = false;
 
 cellElements.forEach((cell, index) => {
     cell.addEventListener("click", () => {
@@ -18,9 +27,9 @@ cellElements.forEach((cell, index) => {
 });
 
 function placeMarker(index) {
-    let col = index % 3
-    let row = (index - col) / 3
-    if(boardData[row][col] == 0 && gameOver == false) {
+    const col = index % 3;
+    const row = Math.floor(index / 3);
+    if (boardData[row][col] === EMPTY && !gameOver) {
         boardData[row][col] = player;
         player *= -1;
         drawMarkers();
@@ -29,74 +38,67 @@ function placeMarker(index) {
 }
 
 function drawMarkers() {
-    for(let row = 0; row < 3; row++) {
-        for(let col = 0; col < 3; col++) {
-            if(boardData[row][col] == 1) {
-                cellElements[(row * 3) + col].classList.add("cross");
-            } else if(boardData[row][col] == -1) {
-                cellElements[(row * 3) + col].classList.add("circle");
+    for (const [row, rowData] of boardData.entries()) {
+        for (const [col, marker] of rowData.entries()) {
+            const cellIndex = row * 3 + col;
+            if (marker === PLAYER_X) {
+                cellElements[cellIndex].classList.add("cross");
+            } else if (marker === PLAYER_O) {
+                cellElements[cellIndex].classList.add("circle");
             }
         }
     }
 }
 
 function checkResult() {
-    for(let i = 0; i < 3; i++) {
-        let rowSum = boardData[i][0] + boardData[i][1] + boardData[i][2];
-        let colSum = boardData[0][i] + boardData[1][i] + boardData[2][i];
-        if(rowSum == 3 || colSum == 3) {
-            endGame(1);
-            return
-        } else if(rowSum == -3 || colSum == -3) {
-            endGame(2);
-            return
-        }
-
-
-        let diagonalSum1 = boardData[0][0] + boardData[1][1] + boardData[2][2];
-        let diagonalSum2 = boardData[0][2] + boardData[1][1] + boardData[2][0];
-        if(diagonalSum1 == 3 || diagonalSum2 == 3) {
-            endGame(1);
-            return
-        } else if(diagonalSum1 == -3 || diagonalSum2 == -3) {
-            endGame(2);
-            return
+    for (let i of [0, 1, 2]) {
+        const rowSum = boardData[i][0] + boardData[i][1] + boardData[i][2];
+        const colSum = boardData[0][i] + boardData[1][i] + boardData[2][i];
+        if (rowSum === 3 || colSum === 3) {
+            endGame(GAME_STATUS.X_WINS);
+            return;
+        } else if (rowSum === -3 || colSum === -3) {
+            endGame(GAME_STATUS.O_WINS);
+            return;
         }
     }
 
-    if(boardData[0].indexOf(0) == -1 &&
-        boardData[1].indexOf(0) == -1 &&
-        boardData[2].indexOf(0) == -1) {
-        endGame(0);
-        return
+    const diagonalSum1 = boardData[0][0] + boardData[1][1] + boardData[2][2];
+    const diagonalSum2 = boardData[0][2] + boardData[1][1] + boardData[2][0];
+    if (diagonalSum1 === 3 || diagonalSum2 === 3) {
+        endGame(GAME_STATUS.X_WINS);
+        return;
+    } else if (diagonalSum1 === -3 || diagonalSum2 === -3) {
+        endGame(GAME_STATUS.O_WINS);
+        return;
+    }
+
+    if (boardData.every(row => row.every(cell => cell !== EMPTY))) {
+        endGame(GAME_STATUS.DRAW);
     }
 }
 
-// Функция окончания игры
-function endGame(winner) {
+function endGame(status) {
     gameOver = true;
-    if(winner == 0) {
-        resultElement.innerText = "Ничья!"
-    } else if (winner == 1) {
-        resultElement.innerText = "Победа крестиков!"
+    if (status === GAME_STATUS.DRAW) {
+        resultElement.innerText = "Ничья!";
+    } else if (status === GAME_STATUS.X_WINS) {
+        resultElement.innerText = "Победа крестиков!";
     } else {
-        resultElement.innerText = "Победа ноликов!"
+        resultElement.innerText = "Победа ноликов!";
     }
 }
 
-// Реванш
-const restartButton = document.getElementById("restart");
 restartButton.addEventListener("click", () => {
     boardData = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-        ]
-        player = 1;
-        gameOver = false;
-        // Сброс игры
-        cellElements.forEach(cell => {
-            cell.classList.remove("cross", "circle");
-        });
-        resultElement.innerText = ""
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
+    ];
+    player = PLAYER_X;
+    gameOver = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove("cross", "circle");
+    });
+    resultElement.innerText = "";
 });
