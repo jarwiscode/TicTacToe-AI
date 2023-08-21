fdc6d352184a426daa0162253aef5ce5d7087fd4
jarwiscode <dz.ibragimov@yandex.ru>
2023-08-21 03:01:11 +0300

    add AI

--- script.js
+++ script.js
@@ -26,16 +26,75 @@ cellElements.forEach((cell, index) => {
     });
 });
 
+function checkWin(player) {
+    for (let i of [0, 1, 2]) {
+        const rowSum = boardData[i][0] + boardData[i][1] + boardData[i][2];
+        const colSum = boardData[0][i] + boardData[1][i] + boardData[2][i];
+        if (rowSum === 3 * player || colSum === 3 * player) {
+            return true;
+        }
+    }
+
+    const diagonalSum1 = boardData[0][0] + boardData[1][1] + boardData[2][2];
+    const diagonalSum2 = boardData[0][2] + boardData[1][1] + boardData[2][0];
+    if (diagonalSum1 === 3 * player || diagonalSum2 === 3 * player) {
+        return true;
+    }
+
+    return false;
+}
+
+
 function placeMarker(index) {
-    const col = index % 3;
-    const row = Math.floor(index / 3);
+    let col = index % 3;
+    let row = Math.floor(index / 3);
     if (boardData[row][col] === EMPTY && !gameOver) {
         boardData[row][col] = player;
         player *= -1;
         drawMarkers();
         checkResult();
+
+        if (!gameOver && player === PLAYER_O) {
+            makeRandomAIMove();
+        }
     }
 }
+
+function makeRandomAIMove() {
+    if (gameOver) return;
+
+    const availableCells = [];
+    const potentialWinningMoves = [];
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (boardData[row][col] === EMPTY) {
+                availableCells.push({ row, col });
+
+                // Проверяем, есть ли у ИИ возможность завершить ряд
+                boardData[row][col] = PLAYER_O;
+                if (checkWin(PLAYER_O)) {
+                    potentialWinningMoves.push({ row, col });
+                }
+                boardData[row][col] = EMPTY;
+            }
+        }
+    }
+
+    if (potentialWinningMoves.length > 0) {
+        const randomIndex = Math.floor(Math.random() * potentialWinningMoves.length);
+        const winningMove = potentialWinningMoves[randomIndex];
+        const index = winningMove.row * 3 + winningMove.col;
+        placeMarker(index);
+    } else if (availableCells.length > 0) {
+        const randomIndex = Math.floor(Math.random() * availableCells.length);
+        const randomMove = availableCells[randomIndex];
+        const index = randomMove.row * 3 + randomMove.col;
+        placeMarker(index);
+    }
+}
+
+
 
 function drawMarkers() {
     for (const [row, rowData] of boardData.entries()) {
